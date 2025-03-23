f8bc6d559ad75006234eb697dc6eac1e942d8310
zennythh <shr0udd.02@gmail.com>
2025-03-23 10:04:51 +0000

    added item inits

--- src/main/java/com/untilifoundyou/lostandfound/item/ItemRepository.java
+++ src/main/java/com/untilifoundyou/lostandfound/item/ItemRepository.java
@@ -1,5 +1,22 @@
 package com.untilifoundyou.lostandfound.item;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import jakarta.annotation.PostConstruct;
+
 public class ItemRepository {
 
+    private List<Item> items = new ArrayList<>();
+    
+    List<Run> findAll(){
+        return items;
+    }
+    
+    @PostConstruct
+    private void init(){
+        items.add(new Item(1, "panyo na blu", LocalDateTime.now(), LocalDateTime.now().minus(1, ChronoUnit.HOURS), ItemStatus.Lost));
+        items.add(new Item(2, "selpon", LocalDateTime.now(), LocalDateTime.now().minus(3, ChronoUnit.HOURS), ItemStatus.Found));
+    }
 }
