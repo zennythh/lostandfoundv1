0b434f13ee33098ea9bc909b29e4c10faef9f278
zennythh <shr0udd.02@gmail.com>
2025-03-26 11:59:14 +0800

    Testing ItemCampus enum class for item sorting

--- src/main/java/com/untilifoundyou/lostandfound/model/Item.java
+++ src/main/java/com/untilifoundyou/lostandfound/model/Item.java
@@ -3,6 +3,5 @@ package com.untilifoundyou.lostandfound.model;
 import jakarta.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
-public record Item (Integer itemID, @NotEmpty String itemName, String itemDesc, String itemLoc, LocalDateTime postedOn, LocalDateTime timeOfConcern, ItemStatus Lost){
-
+public record Item (Integer itemID, @NotEmpty String itemName, String itemDesc, String itemLoc, ItemCampus Campus, LocalDateTime postedOn, LocalDateTime timeOfConcern, ItemStatus Status){
 }
