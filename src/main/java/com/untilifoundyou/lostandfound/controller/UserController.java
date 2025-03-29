2cd5ca4e06f20f78b5a6bd4803bd016633068531
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2025-03-29 15:55:35 +0800

    crud for user

--- src/main/java/com/untilifoundyou/lostandfound/controller/UserController.java
+++ src/main/java/com/untilifoundyou/lostandfound/controller/UserController.java
@@ -5,6 +5,12 @@ import com.untilifoundyou.lostandfound.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,8 +27,42 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @PostMapping
+    // getUser
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+    
+
+    // create user
+    @PostMapping("/create")
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
     }
+
+    // update user
+    @PutMapping("/update/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        User existingUser = userRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid user Id: " + id));
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setMiddleName(user.getMiddleName());
+        return userRepository.save(existingUser);
+    }
+
+
+    // delete user
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        User user = userRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid user Id: " + id));
+        userRepository.delete(user);
+        return "Delete success!";
+    }
+
+
+    
+    
+
 }
