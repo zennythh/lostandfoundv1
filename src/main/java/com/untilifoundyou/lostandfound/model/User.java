d1eb118a76bff30f3541395ef3b77514e8c2de8d
zennythh <shr0udd.02@gmail.com>
2025-04-01 12:02:13 +0800

    Fixed the User class

--- src/main/java/com/untilifoundyou/lostandfound/model/User.java
+++ src/main/java/com/untilifoundyou/lostandfound/model/User.java
@@ -1,5 +1,6 @@
 package com.untilifoundyou.lostandfound.model;
 
+import com.untilifoundyou.lostandfound.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.ArrayList;
@@ -25,6 +26,16 @@ public class User {
 
     @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
 
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Item> items = new ArrayList<>();
